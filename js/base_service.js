9a98b4831ab6677153c03340af4f6eea1368b5e0
Esmuellert1325 <faanandor@gmail.com>
2023-10-03 17:05:32 +0200

    Commiting state before new Home CSS

--- js/base_service.js
+++ js/base_service.js
@@ -10,21 +10,5 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('Clicked');
 
         window.location = 'http://localhost/php-blog/logout';
-        /*
-        fetch(url, {
-            method: 'POST'
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                else {
-                    window.location = 'http://localhost/php-blog/login';
-                }
-            })
-            .catch((error) => {
-                console.error('There was a problem with the fetch operation:', error);
-            });
-            */
     }
 });
